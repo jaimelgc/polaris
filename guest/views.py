8b59ebcf26dd70195d6d53454765c4aaf3cc9fd5
jaimelgc <jaimeleongarcia123@gmail.com>
2023-10-29 12:15:52 +0000

    vista producto

--- guest/views.py
+++ guest/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse  # TWT ?
 
-# Create your views here.
+# not in use
+# from django.shortcuts import render
+
+# Nombre placeholder ?
+
+
+def index(response, id):
+    return HttpResponse("<h1>%s</h1>" % id)
