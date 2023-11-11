911f2dc9310b845a690ef96e4e0fe5eded6f330c
javigh193pcclase <javigh1903@movistar.es>
2023-11-11 17:03:14 +0000

    añadido atributo 'kind' al modelo de 'transaction'

--- transactions/models.py
+++ transactions/models.py
@@ -4,12 +4,18 @@ from client.models import Account
 
 
 class Transaction(models.Model):
+    class Type(models.TextChoices):
+        INCOMING = "INC", "Incomming"
+        OUTGOING = "OUT", "Outgoing"
+        PAYMENT = "PAY", "Payment"
+        COMMISSION = "COM", "Commission"
+
     agent = models.CharField(max_length=120)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     concept = models.CharField(max_length=120)
     timestamp = models.DateTimeField(auto_now_add=True)
     account = models.ForeignKey(Account, related_name='transactions', on_delete=models.CASCADE)
-    # Falta kind
+    kind = models.CharField(max_length=3, choices=Type.choices)
 
     def __str__(self):
         return f'Transaction {self.id} - {self.amount}'
